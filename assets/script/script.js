85240a1b5d207c045f350fc09aee8518c76ef325
Agha Chukwuemeka <chukwuemekaagha733@gmail.com>
2022-09-07 15:17:57 +0100

    Created an assets folder

---
+++ assets/script/script.js
@@ -0,0 +1,15 @@
+// Adding the typed.js script
+var auto = new Typed('.text-slider', {
+  strings: [
+    'Welcome!!!',
+    "you're stucked?",
+    "let's help find your way back",
+    'click the button to get started'
+  ],
+  typeSpeed: 200,
+  backSpeed: 150,
+  loop: true,
+  showCursor: true,
+  startDelay: 150,
+  backDelay: 500
+})
