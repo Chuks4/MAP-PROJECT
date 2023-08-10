52aa83ee5339780d34fdf8133bf46649084b19ec
Agha Chukwuemeka <chukwuemekaagha733@gmail.com>
2023-08-10 22:24:41 +0100

    Added my map object

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,15 +1,26 @@
-// Adding the typed.js script
-var auto = new Typed('.text-slider', {
-  strings: [
-    'Welcome!!!',
-    "you're stucked?",
-    "let's help find your way back",
-    'click the button to get started'
-  ],
-  typeSpeed: 200,
-  backSpeed: 150,
-  loop: true,
-  showCursor: true,
-  startDelay: 150,
-  backDelay: 500
-})
+// TOMTOM MAP-SDK PROJECT KEY
+let apikey = "DhsWrXMltRzsirhtAYa3GBdt8j4mhGBb";
+let center = [8.6753, 9.082];
+
+// CREATING A NEW MAP OBJECT
+let map = tt.map({
+  key: apikey,
+  container: "map",
+  minZoom: 10,
+  maxZoom: 15,
+  center: center,
+});
+
+// ADDING A MARKER TO THE MAP
+let marker = new tt.Marker({
+  // DOM OF THE MARKER TO BE DISPLAYED
+  element: document.querySelector("#marker"),
+  anchor: "bottom-right",
+});
+
+// SETTING A POSITION FOR OUR MARKER
+marker.setLngLat([7.5248, 5.4527]);
+
+// ATTACHING THE MAKER ON OUR MAP
+marker.addTo(map);
+
